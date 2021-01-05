02d843d5253a5b013fd5a00f4a06380e518f92a2
atlfinesse <finesse.cap@gmail.com>
2021-01-05 14:10:50 -0800

    Started creating form

--- frontend/src/components/New/New.js
+++ frontend/src/components/New/New.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
+import './New.css'
 
 const New = () => {
+    const [data, setData] = useState({
+        prompt: '',
+        answer: '',
+        topic: [],
+        category: '',
+        resource: '',
+        example: '',
+    })
+
+    const [message, setMessage] = useState()
+
+    const handleChange = (event) => {
+        event.preventDefault()
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const url = 'http://localhost:8000/api/questions/'
+        console.log('submitted')
+        axios({
+            method: 'post',
+            url: url,
+            data: data
+        })
+            .then((res) => {
+                
+            })
+    }
+
+    const showModal = () => {
+        setMessage(!message)
+        console.log('clicked')
+    }
+
+    console.log(message)
+
     return (
         <div>
             New Page
-            <form>
+            {message === true  ? (
+                <div className='modal'>Successfully Submitted/Failed Submission</div>
+            ) : null}
+            <button onClick={showModal}>show modal</button>
+            <form onSubmit={handleSubmit}>
+                <input></input>
                 <button>Submit</button>
             </form>
         </div>
