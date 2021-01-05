8088632b3930070f86e0fc8197325164ea98e4fe
atlfinesse <finesse.cap@gmail.com>
2021-01-05 00:26:02 -0800

    Started logic for get request

--- frontend/src/components/Frontend/Frontend.js
+++ frontend/src/components/Frontend/Frontend.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios'
 
 const Frontend = () => {
+
+    useEffect(() => {
+        const url = 'http://localhost:8000';
+
+        axios({
+            method: 'get',
+            url: url
+        })
+        .then(console.log)
+    }, [])
     return (
         <div>
             Frontend Page
