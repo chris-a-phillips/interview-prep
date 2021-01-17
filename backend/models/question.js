2fc7cf66325d5ce0e44cdba16d3279cf93d301f4
atlfinesse <finesse.cap@gmail.com>
2021-01-16 16:00:06 -0800

    Started css on new question page

--- backend/models/question.js
+++ backend/models/question.js
@@ -1,13 +1,13 @@
 const mongoose = require('../db/connection');
 
 const QuestionSchema = new mongoose.Schema({
-    prompt: String,
-    answer: String,
-    category: String,
-    // frontend, backend, whiteboard, behavioral, code-challenge
-    topic: [String],
-    resource: String,
-    example: String
+	prompt: { type: String, required: true },
+	answer: { type: String, required: true },
+	category: String,
+	// frontend, backend, whiteboard, behavioral, code-challenge
+	topic: { type: [String], required: true },
+	resource: { type: String, required: true },
+	example: String,
 });
 
 const Question = mongoose.model('Question', QuestionSchema);
